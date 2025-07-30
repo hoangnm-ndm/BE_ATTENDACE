6af69f6bfc41e1791a87a32cdc138813a41269be
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-07-30 09:16:06 +0700

    init

---
+++ src/modules/attendance/attendance.routes.js
@@ -0,0 +1,5 @@
+import Router from "express";
+
+const attendaceRoutes = Router();
+
+export default attendaceRoutes;
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import Router from "express";
+import authRoutes from "./modules/auth/auth.routes";
+import attendaceRoutes from "./modules/attendance/attendance.routes";
+import classRoutes from "./modules/class/class.routes";
+import majorRoutes from "./modules/major/major.routes";
+import sessionRoutes from "./modules/session/session.routes";
+import subjectRoutes from "./modules/subject/subject.routes";
+import userRoutes from "./modules/user/user.routes";
+
+const routes = Router();
+
+routes.use("/auth", authRoutes);
+routes.use("/attendances", attendaceRoutes);
+routes.use("/classes", classRoutes);
+routes.use("/majors", majorRoutes);
+routes.use("/sessions", sessionRoutes);
+routes.use("/subjects", subjectRoutes);
+routes.use("/users", userRoutes);
+
+export default routes;
