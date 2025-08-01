633e7c8f198e3e14dadd4ca294c2cf70652bf124
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-08-01 11:36:45 +0700

    run swagger

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,6 +1,6 @@
-import { FRONTEND_URL, RESET_PASSWORD_EXPIRES, RESET_PASSWORD_SECRET } from "../../common/configs/environment.js";
+import { FRONTEND_URL, RESET_PASSWORD_EXPIRES } from "../../common/configs/environment.js";
 import { generateStudentId, generateUsername } from "../../common/utils/code-generator.js";
-import { createError } from "../../common/utils/create-error.js";
+import { throwError } from "../../common/utils/create-error.js";
 import { signToken, verifyToken } from "../../common/utils/jwt.js";
 import { comparePassword, hashPassword } from "../../common/utils/password-handler.js";
 import sendEmail from "../../common/utils/send-email.js";
@@ -14,7 +14,7 @@ export const registerSevice = async (dataRegister) => {
 	//* 1. Kiểm tra email người dùng đã đăng ký chưa?
 	const existingUser = await User.findOne({ email });
 	if (existingUser) {
-		return next(createError(400, MESSAGES.EMAIL_ALREADY_EXISTS));
+		return throwError(400, MESSAGES.EMAIL_ALREADY_EXISTS);
 	}
 
 	//* 2. Mã hoá mật khẩu
@@ -44,10 +44,10 @@ export const loginService = async (dataLogin) => {
 	const { email, password } = dataLogin;
 
 	const user = await User.findOne({ email });
-	if (!user) return next(createError(401, MESSAGES.USER_NOT_FOUND));
+	if (!user) throwError(401, MESSAGES.USER_NOT_FOUND);
 
 	const isMatch = comparePassword(password, user.password);
-	if (!isMatch) return next(createError(401, MESSAGES.INVALID_PASSWORD));
+	if (!isMatch) throwError(401, MESSAGES.INVALID_PASSWORD);
 
 	const accessToken = signToken({ id: user._id }, "1d");
 	const refreshToken = signToken({ id: user._id }, "30d");
@@ -63,9 +63,7 @@ export const loginService = async (dataLogin) => {
 
 export const refreshTokenService = async (refreshToken) => {
 	console.log(refreshToken);
-	if (!refreshToken) {
-		return createError(401, MESSAGES.INVALID_REFRESH_TOKEN);
-	}
+	if (!refreshToken) throwError(401, MESSAGES.INVALID_REFRESH_TOKEN);
 
 	const { valid, decoded } = verifyToken(refreshToken);
 	if (valid) {
@@ -77,7 +75,7 @@ export const refreshTokenService = async (refreshToken) => {
 
 export const fotgotPasswordService = async (email) => {
 	const user = await User.findOne({ email });
-	if (!user) return next(createError(404, MESSAGES.USER_NOT_FOUND));
+	if (!user) throwError(404, MESSAGES.USER_NOT_FOUND);
 
 	// * 1. Gửi mail cho họ với 1 OTP (One-time-password) có 6 chữ số ngẫu nhiên.
 	// * 2. Gửi cho họ link chứa mã bí mật, link dẫn đến FE (có trải nghiệm người dùng tốt hơn vì người dùng không phải nhập các số ngẫu nhiên, an toàn hơn vì những sơ xuất của người dùng, không thực sự hỗ trợ tốt cho đa nền tảng)
@@ -89,25 +87,21 @@ export const fotgotPasswordService = async (email) => {
 
 	const resetLink = `${FRONTEND_URL}/auth/reset-password/${resetToken}`;
 	const subject = "[CodeFarm] Đặt lại mật khẩu cho tài khoản của bạn";
-	const html = generateResetPasswordEmail(resetLink, (expiresIn = "15 phút"));
+	const html = generateResetPasswordEmail(resetLink, RESET_PASSWORD_EXPIRES || "15 phút");
 	await sendEmail(email, subject, { html });
 	return true;
 };
 
 export const resetPasswordService = async (resetToken, newPassword) => {
 	const decoded = verifyToken(resetToken);
-
-	console.log(decoded);
 	const user = await User.findById(decoded.decoded.id);
-	console.log(user);
-
-	if (!user) return false;
-
+	if (!user) throwError(400, MESSAGES.USER_NOT_FOUND);
 	user.password = await hashPassword(newPassword);
-	await user.save();
 
+	await user.save();
 	const subject = "Mật khẩu đã được đặt lại";
 	const html = generatePasswordResetSuccessEmail();
 	await sendEmail(user.email, subject, { html });
+
 	return true;
 };
